86da40e92cc65b53fab6cacaab34483c47fe6119
Brad Richardson <brichardson@structint.com>
2017-04-01 07:23:57 -0700

    Add some notes to help clarify my thought process

--- Chapter1/Section1.3/Exercise1.29.scm
+++ Chapter1/Section1.3/Exercise1.29.scm
@@ -15,4 +15,8 @@
     (define (term y) (+ (* 4.0 (f y)) (* 2.0 (f (+ y h)))))
     (* (/ h 3.0) (+ (f a) (f b) (sum term (+ a h) next (- b h)))))
 
-; h/3[f(a) + 4f(a+h) + 2f(a+2h) + ... + 4f(a+(n-2)h) + 2f(a+(n-1)h) + f(a+nh)]
+; h/3[y0 + 4y1 + 2y2 + 4y3 + 2y4 + ... + 4yn-2 + 2yn-1 + yn]
+; h/3[f(a) + f(a+nh) + 4f(a+h) + 2f(a+2h) + ... + 4f(a+(n-2)h) + 2f(a+(n-1)h)]
+; h/3[f(a) + f(b) + 4f(a+h) + 2f(a+2h) + ... + 4f(a+(n-2)h) + 2f(a+(n-1)h)]
+; jm = 4f(a+k) + 2f(a+k+h); k=2mh; m=n/2 m=1 to n/2-1
+; h/3[f(a) + f(b) + {j1 + j2 + ... + jm-1 + jm}]
