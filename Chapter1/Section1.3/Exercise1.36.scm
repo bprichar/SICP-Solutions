4d5ccaf0fdb27adde6f03ade5239dc95bcf3bf7b
Brad Richardson <brichardson@structint.com>
2017-04-02 17:15:27 -0700

    Solve exercise 1.36

---
+++ Chapter1/Section1.3/Exercise1.36.scm
@@ -0,0 +1,28 @@
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+    (define (close-enough? v1 v2)
+        (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+        (display "x = ")
+        (display guess)
+        (newline)
+        (let ((next (f guess)))
+            (if (close-enough? guess next)
+                next
+                (try next))))
+    (try first-guess))
+
+(define (avg-damped f first-guess)
+    (define (close-enough? v1 v2)
+        (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+        (display "x = ")
+        (display guess)
+        (newline)
+        (let ((next (/ (+ guess (f guess)) 2)))
+            (if (close-enough? guess next)
+                next
+                (try next))))
+    (try first-guess))
+
+; avg-damped converges much faster for x = log(1000)/log(x)
